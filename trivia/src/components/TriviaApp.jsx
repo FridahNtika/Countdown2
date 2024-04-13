301b348186d64902b2de5bde116a7a58c5059834
FridahNtika <fn101@wellesley.edu>
2024-04-13 15:03:55 -0400

    Setting up the questions

--- trivia/src/components/TriviaApp.jsx
+++ trivia/src/components/TriviaApp.jsx
@@ -1,5 +1,24 @@
 import React, { useEffect } from 'react'
 
 const TriviaApp = () => {
-    
+    const [question, setQuestions] = useState(null);
+
+    useEffect(() => {
+        //fetch trivia data
+        const fetchData = async () => {
+            const result = await fetch('');
+            const resultJson = await result.json()
+            setQuestions(resultJson.results)
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <>
+        <h1>Trivia App</h1>
+        {setQuestions.mapp((question, index) => [
+            <p>{question.question}</p>
+        ])}
+        </>
+    )
 }
